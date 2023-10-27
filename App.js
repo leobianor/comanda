3f0340b5201b984a8ab5a00220e8d68de1bd2243
leobianor <89613674+leobianor@users.noreply.github.com>
2023-10-27 08:23:22 -0300

    API mais paginas de login e produto

--- App.js
+++ App.js
@@ -1,52 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Comanda from './screens/comanda';
+import Login from './screens/login';
+import Produtos from './screens/produtos';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.imagem} source={require('./assets/burguer.jpg')}/>
-      <Text>Sistema</Text>
-      <Text style={styles.titulo} >Comanda</Text>
-      <TextInput style={styles.input} placeholder='e-mail' /> 
-      <TextInput style={styles.input} placeholder='senha' secureTextEntry /> 
-      <Text style={styles.botao} >Entrar</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}> 
+        <Stack.Screen name="Home" component={Login} />
+        <Stack.Screen name="Comanda" component={Comanda} />
+        <Stack.Screen name="Produtos" component={Produtos} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FCFBFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titulo:{
-    color:'#121A2C',
-    fontSize: 32,
-    fontWeight: 'bold'
-  },
-  input:{
-    paddingVertical: 12,
-    paddingHorizontal: 17,
-    borderRadius: 4,
-    borderColor: '#747474',
-    borderWidth: 1,
-    marginVertical: 20
-  },
-  botao:{
-    backgroundColor: '#121A2C',
-    color: '#FFBA26',
-    paddingVertical: 10,
-    paddingHorizontal: 60,
-    borderRadius: 4
-  },
-  imagem:{
-    width:'100%',
-    height: 200,
-    position: 'absolute',
-    top: 0
-  }
-});
---
+++ screens/produtos.jsx
@@ -0,0 +1,64 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { styles } from '../style.js';
+import CardProduto from '../components/card.js';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+
+export default function Produtos() {
+    const [produtos, setProdutos] = useState([])
+
+    useEffect( () => {
+        axios.get("http://localhost:3000/produtos")
+            .then(resp => setProdutos(resp.data))
+
+    }, [] )
+
+    return (
+        <View style={styles.containerBetween}>
+            <View style={styles.header}>
+                <MaterialIcons name="arrow-back" size={24} color="black" />
+
+                <View style={styles.box}>
+                    <Text>comanda</Text>
+                    <Text style={styles.title}>2457</Text>
+                </View>
+            </View>
+
+            <View style={styles.pedidos}>
+                <Text style={styles.subtitle}>Pedido</Text>
+                <View style={styles.pedidoData}>
+                    <Text>Saldo da comanda</Text>
+                    <Text>R$ 0,00</Text>
+                </View>
+                <View style={styles.pedidoData}>
+                    <Text>Total do pedido</Text>
+                    <Text>R$ +20,00</Text>
+                </View>
+                <View style={styles.pedidoDataTotal}>
+                    <Text>Saldo final</Text>
+                    <Text>R$ 149,00</Text>
+                </View>
+            </View>
+
+            <TextInput style={styles.input} placeholder="procurar" />
+
+            <Text style={styles.subtitle}>Produtos</Text>
+
+            <ScrollView style={styles.scroll}>
+                {produtos.map(p => <CardProduto key={p.id} produto={p} />)}
+
+            </ScrollView>
+
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>finalizar</Text>
+            </TouchableOpacity>
+        </View>
+
+    )
+}
